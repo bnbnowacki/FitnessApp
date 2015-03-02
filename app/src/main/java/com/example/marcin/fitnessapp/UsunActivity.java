b27d4782ac34e8440f7c86c7ae56de45b58cb0bf
bnbnowacki <bnbnowacki@gmail.com>
2015-03-02 20:16:56 +0100

    Funkcje działające: -lista ćwiczeń, -usuwanie ćwiczenia, -dodawanie ćwiczenia

--- app/src/main/java/com/example/marcin/fitnessapp/UsunActivity.java
+++ app/src/main/java/com/example/marcin/fitnessapp/UsunActivity.java
@@ -1,12 +1,18 @@
 package com.example.marcin.fitnessapp;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
 public class UsunActivity extends ActionBarActivity {
@@ -15,13 +21,27 @@ public class UsunActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usun);
-        ListView lista1 = (ListView)findViewById(R.id.listView);
-        DbHandler dbHelp=new DbHandler(getApplicationContext());
+        ListView lista1 = (ListView)findViewById(R.id.ListView);
+        final DbHandler dbHelp=new DbHandler(getApplicationContext());
         String kolumny[]= {DbHandler.COLUMN_NAME_NAZWA, DbHandler.COLUMN_NAME_KALORIE};
         int[] widoki={R.id.textViewNazwa, R.id.textViewKalorie};
         Cursor c = dbHelp.WyswietlWszystkie(dbHelp);
         SimpleCursorAdapter adapter1 = new SimpleCursorAdapter(getApplicationContext(), R.layout.list_item, c, kolumny, widoki, 0);
         lista1.setAdapter(adapter1);
+        lista1.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView listView, View view, int position, long id){
+                Cursor cursor =(Cursor)listView.getItemAtPosition(position);
+                String nazwa = cursor.getString(1);
+                Toast.makeText(getApplicationContext(), "Usunięto: "+nazwa, Toast.LENGTH_LONG).show();
+                SQLiteDatabase db = dbHelp.getWritableDatabase();
+                String where = DbHandler.COLUMN_NAME_NAZWA+" = ?";
+                String[] whereArgs={cursor.getString(1)};
+                db.delete(DbHandler.TABLE_NAME, where, whereArgs);
+                finish();
+            }
+        });
+
     }
 
 
